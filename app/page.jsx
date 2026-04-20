ceb26ebe273aedccd66589702028eb4d7cf7ab92
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2026-04-19 23:24:36 -0700

    Refactor page structure and add analytics script

--- app/page.jsx
+++ app/page.jsx
@@ -1,62 +1,65 @@
-import Script from "next/script";  // Add this import
+import Script from "next/script";
 import {Button} from "@/components/ui/button";
 import Socials from "@/components/ui/Socials";
 import Photo from "@/components/ui/Photo";
 import Stats from "@/components/ui/Stats";
 import {FiDownload} from "react-icons/fi";
+
 const Home = () => {
   return (
-    {/* Ahrefs Analytics Script */}
+    <>
+      {/* Ahrefs Analytics Script */}
       <Script
         src="https://analytics.ahrefs.com/analytics.js"
         data-key="5rMm0tny781JSH7P+/ZD+w"
         strategy="afterInteractive"
         async
       />
-          
-    <section className="h-full">
-      <div className='container mx-auto h-full'>
-        <div className="flex flex-col xl:flex-row items-center justify-between 
-        xl:pt-8 xl:pb-24">
-          {/* text */}
-          <div className="text-center xl:text-left order-2 xl:order-none flex-1">
-            <span className="text-xl">Web Developer</span>
-            <h1 className="h1">
-              Hello I'm <br/> <span className="text-[var(--textaccent)]">Ronvic Cuevas</span>
-            </h1>
-            <p className="max-w-[500px] mb-9 text-white/80">
-            I strive to create effective solutions and am always looking to grow my skills. 
-            I'm comfortable working with a range of programming languages and technologies, and I enjoy learning new things. 
-            I value collaboration and believe in working together to achieve the best results.
-            </p>
-            {/*Button and Socials*/}
-            <div className="flex flex-col xl:flex-row items-center gap-8">
-              <a href="/assets/MyCV.pdf" download>
-              <Button
-              variant="outline"
-              size="lg"
-              className="uppercase flex items-center gap-2"
-              >
-                <span>Download CV</span>
-                <FiDownload className="text-xl"/>
-              </Button>
-              </a>
-              <div className="mb-8 xl:mb-0">
-                <Socials containerStyles="flex gap-6" 
-                iconStyles={"w-9 h-9 border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"}
-                />
+
+      <section className="h-full">
+        <div className='container mx-auto h-full'>
+          <div className="flex flex-col xl:flex-row items-center justify-between 
+          xl:pt-8 xl:pb-24">
+            {/* text */}
+            <div className="text-center xl:text-left order-2 xl:order-none flex-1">
+              <span className="text-xl">Web Developer</span>
+              <h1 className="h1">
+                Hello I'm <br/> <span className="text-[var(--textaccent)]">Ronvic Cuevas</span>
+              </h1>
+              <p className="max-w-[500px] mb-9 text-white/80">
+              I strive to create effective solutions and am always looking to grow my skills. 
+              I'm comfortable working with a range of programming languages and technologies, and I enjoy learning new things. 
+              I value collaboration and believe in working together to achieve the best results.
+              </p>
+              {/*Button and Socials*/}
+              <div className="flex flex-col xl:flex-row items-center gap-8">
+                <a href="/assets/MyCV.pdf" download>
+                  <Button
+                    variant="outline"
+                    size="lg"
+                    className="uppercase flex items-center gap-2"
+                  >
+                    <span>Download CV</span>
+                    <FiDownload className="text-xl"/>
+                  </Button>
+                </a>
+                <div className="mb-8 xl:mb-0">
+                  <Socials containerStyles="flex gap-6" 
+                  iconStyles={"w-9 h-9 border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-           {/* photo */}
+            {/* photo */}
             <div className="order-1 xl:order-none mb-8 xl:mb-0">
-            <Photo/> 
+              <Photo/> 
             </div>             
+          </div>
         </div>
-      </div>
-      <Stats/>
-    </section>
+        <Stats/>
+      </section>
+    </>
   );
 };
 
-export default Home
+export default Home;
